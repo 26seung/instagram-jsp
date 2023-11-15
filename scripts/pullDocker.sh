4e20fd463a0259050db6dcbc11f334725e1e88db
Euseung <yousi7@naver.com>
2023-11-16 01:04:46 +0900

    test

--- scripts/pullDocker.sh
+++ scripts/pullDocker.sh
@@ -1,14 +1,16 @@
+cd /home/ubuntu/deploy
 # AWS CLI
-aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
+#aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
+aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 076796029005.dkr.ecr.ap-northeast-2.amazonaws.com
 
 # pull docker image
-if [ "$DEPLOYMENT_GROUP_NAME" == "dev" ]
-then
-  docker-compose -f /deploy/docker-compose.dev.yml pull
-elif [ "$DEPLOYMENT_GROUP_NAME" == "stage" ]
-then
-  docker-compose -f /deploy/docker-compose.stage.yml pull
-elif [ "$DEPLOYMENT_GROUP_NAME" == "production" ]
-then
-  docker-compose -f /deploy/docker-compose.yml pull
-fi
+#if [ "$DEPLOYMENT_GROUP_NAME" == "dev" ]
+#then
+#  docker-compose -f /deploy/docker-compose.dev.yml pull
+#elif [ "$DEPLOYMENT_GROUP_NAME" == "stage" ]
+#then
+#  docker-compose -f /deploy/docker-compose.stage.yml pull
+#elif [ "$DEPLOYMENT_GROUP_NAME" == "production" ]
+#then
+  docker-compose -f docker-compose.yml pull
+#fi
--- scripts/runDocker.sh
+++ scripts/runDocker.sh
@@ -1,19 +1,21 @@
-if [ "$DEPLOYMENT_GROUP_NAME" == "dev" ]
-then
+cd /home/ubuntu/deploy
+
+#if [ "$DEPLOYMENT_GROUP_NAME" == "dev" ]
+#then
+#  # Remove any anonymous volumes attached to containers
+#  docker-compose -f /deploy/docker-compose.dev.yml rm -v
+#  # build images and run containers
+#  docker-compose -f /deploy/docker-compose.dev.yml up --detach --renew-anon-volumes
+#elif [ "$DEPLOYMENT_GROUP_NAME" == "stage" ]
+#then
+#  # Remove any anonymous volumes attached to containers
+#  docker-compose -f /deploy/docker-compose.stage.yml rm -v
+#  # build images and run containers
+#  docker-compose -f /deploy/docker-compose.stage.yml up --detach --renew-anon-volumes
+#elif [ "$DEPLOYMENT_GROUP_NAME" == "production" ]
+#then
   # Remove any anonymous volumes attached to containers
-  docker-compose -f /deploy/docker-compose.dev.yml rm -v
+  docker-compose -f docker-compose.yml rm
   # build images and run containers
-  docker-compose -f /deploy/docker-compose.dev.yml up --detach --renew-anon-volumes
-elif [ "$DEPLOYMENT_GROUP_NAME" == "stage" ]
-then
-  # Remove any anonymous volumes attached to containers
-  docker-compose -f /deploy/docker-compose.stage.yml rm -v
-  # build images and run containers
-  docker-compose -f /deploy/docker-compose.stage.yml up --detach --renew-anon-volumes
-elif [ "$DEPLOYMENT_GROUP_NAME" == "production" ]
-then
-  # Remove any anonymous volumes attached to containers
-  docker-compose -f /deploy/docker-compose.yml rm -v
-  # build images and run containers
-  docker-compose -f /deploy/docker-compose.yml up --detach --renew-anon-volumes
-fi
+  docker-compose -f docker-compose.yml up -d
+#fi
