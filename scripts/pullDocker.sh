7d274f97f1b844ce13093af76b80a73d37cca5ef
Euseung <yousi7@naver.com>
2023-11-18 02:53:20 +0900

    test

--- scripts/pullDocker.sh
+++ scripts/pullDocker.sh
@@ -15,5 +15,6 @@ aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS
 #  docker-compose -f /deploy/docker-compose.stage.yml pull
 #elif [ "$DEPLOYMENT_GROUP_NAME" == "production" ]
 #then
+  docker rmi -f 076796029005.dkr.ecr.ap-northeast-2.amazonaws.com/photo-ecr
   docker-compose -f docker-compose.yml pull
 #fi
