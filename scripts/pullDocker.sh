cebc5099136d8281e02577deb4d075af86a8c248
Euseung <yousi7@naver.com>
2023-11-19 01:14:21 +0900

    te

--- scripts/pullDocker.sh
+++ scripts/pullDocker.sh
@@ -3,8 +3,11 @@ cd /home/ubuntu/deploy
 echo pullDocker start...
 pwd
 ls
+echo $ECR_REPO
+echo ${ECR_REPO}
 #aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 076796029005.dkr.ecr.ap-northeast-2.amazonaws.com
-aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
+#aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
+$ECR_REPO
 # pull docker image
 #if [ "$DEPLOYMENT_GROUP_NAME" == "dev" ]
 #then
