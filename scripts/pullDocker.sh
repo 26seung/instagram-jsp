897212c9d647c785f4aed70a26314027119df7fa
Euseung <yousi7@naver.com>
2023-11-19 20:12:29 +0900

    te

--- scripts/pullDocker.sh
+++ scripts/pullDocker.sh
@@ -3,7 +3,7 @@ cd /home/ubuntu/deploy
 echo pullDocker start...
 pwd
 ls
-ECR_REPE = "$ECR_REPO"
+ECR_LOGIN=$(aws ssm get-parameter --name /ec2/photo/ecr_repo --query 'Parameter.Value' --output text --with-decryption)
 echo ECR_REPO1 : $ECR_REPO
 echo ECR_REPO2 : "$ECR_REPO"
 echo ECR_REPO3 : ${ECR_REPO}
@@ -12,7 +12,7 @@ echo deploy : "$DEPLOYMENT_GROUP_NAME"
 echo deploy2 : $DEPLOYMENT_GROUP_NAME
 #aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 076796029005.dkr.ecr.ap-northeast-2.amazonaws.com
 #aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
-$ECR_REPO
+$ECR_LOGIN
 # pull docker image
 #if [ "$DEPLOYMENT_GROUP_NAME" == "dev" ]
 #then
